ef2756561e179075eb987562cbd3d6e0cca9792e
Alex Lukomsky <lukomsky.alexander@gmail.com>
2022-10-13 19:16:10 +0300

    example websocket, chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ws, setWs] = useState<WebSocket | null>(null)
+  const [messageText, setMessageText] = useState('')
+  const [usersMessages, setUsersMessages] = useState<usersMessagesType[]>([])
+  if (ws) {
+    ws.onmessage = (messageEvent) => {
+      const messages = JSON.parse(messageEvent.data)
+      setUsersMessages([...usersMessages, ...messages])
+      window.scrollTo(0, document.body.scrollHeight);
+    }
+  }
+  useEffect(() => {
+    const localWs = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+    setWs(localWs)
+  }, [])
+
+  const sendMessage = () => {
+    ws && ws.send(messageText)
+    setMessageText('')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input type="text" value={messageText} onChange={(e) => setMessageText(e.currentTarget.value)} />
+        <button onClick={sendMessage}>SEND MESSAGE</button>
+      </div>
+      {
+        usersMessages.map((m, i) => (
+          <div key={i} style={{ border: '1px solid #f5f5f5', width: 'fit-content', padding: '10px', margin: '0 auto' }}>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }}>
+              <img style={{ width: '70px' }} src={m.photo ? m.photo : ''} alt="" /> <span>{m.userName}</span>
+            </div>
+            <h3>{m.message}</h3>
+          </div>
+
+        ))
+      }
+
     </div>
   );
 }
 
 export default App;
+type usersMessagesType = { userId: number, userName: string, message: string, photo: null | string }
